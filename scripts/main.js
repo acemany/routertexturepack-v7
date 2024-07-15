c779f98a3e3e930d3eb558ded0c93d84231e3168
Acemany <116539353+acemany@users.noreply.github.com>
2024-07-15 21:44:02 +0300

    why not

--- scripts/main.js
+++ scripts/main.js
@@ -1,5 +1,7 @@
 Events.on(EventType.ClientLoadEvent, e=>{
     Vars.content.bullets().each(b => b instanceof BasicBulletType, b => b.frontRegion = Core.atlas.find("router"));
+
+    Core.bundle.debug("")
     });
 // BulletType
     
